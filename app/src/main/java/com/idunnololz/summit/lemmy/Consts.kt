a49a10852d9af948748d4b03bc6692f56d7362b2
Gary Guo <garyguo9@gmail.com>
2026-05-20 11:57:17 -0400

    Change default instance back to lemmy.world
    Changelog is hosted on lemmy.world so this just improves the user experience.

--- app/src/main/java/com/idunnololz/summit/lemmy/Consts.kt
+++ app/src/main/java/com/idunnololz/summit/lemmy/Consts.kt
@@ -3,8 +3,9 @@ package com.idunnololz.summit.lemmy
 import com.idunnololz.summit.util.consts.LEMMY_INSTANCES
 
 object Consts {
-  const val DEFAULT_INSTANCE = "lemmy.zip"
   const val INSTANCE_LEMMY_WORLD = "lemmy.world"
+
+  const val DEFAULT_INSTANCE = INSTANCE_LEMMY_WORLD
 
   val DEFAULT_LEMMY_INSTANCES =
     (LEMMY_INSTANCES + listOf(
