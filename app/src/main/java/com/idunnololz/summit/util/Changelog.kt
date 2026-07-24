e5e707148558dd418957d9c9d0454a6dd45a7e97
Gary Guo <garyguo9@gmail.com>
2026-07-24 04:38:58 -0400

    v1.83.0

--- app/src/main/java/com/idunnololz/summit/util/Changelog.kt
+++ app/src/main/java/com/idunnololz/summit/util/Changelog.kt
@@ -8,7 +8,7 @@ import com.idunnololz.summit.main.MainActivity
  */
 
 val changeLogPostRef
-  get() = PostRef("lemmy.world", 46998220)
+  get() = PostRef("lemmy.world", 49828281)
 
 fun MainActivity.launchChangelog() {
   launchPage(changeLogPostRef, switchToNativeInstance = true)
