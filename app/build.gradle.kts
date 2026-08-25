e40cb8673acb3d51a1866dd48cc0d93dbb64c624
Gary Guo <garyguo9@gmail.com>
2026-08-25 03:44:31 -0400

    v1.84.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "com.idunnololz.summit"
     minSdk = 23
     targetSdk = 37
-    versionCode = 348
-    versionName = "1.84.0"
+    versionCode = 350
+    versionName = "1.84.2"
 
     buildConfigField(
       "String",
